f65c5c7f972b7a37f5063ef21bfe8d213c830974
inerplat <inerplat@gmail.com>
2020-05-06 02:18:41 +0900

    clear jupyter notebook output

--- 1. Preprocessing/faceCrop.py
+++ 1. Preprocessing/faceCrop.py
@@ -3,46 +3,43 @@ import numpy as np
 from matplotlib import pyplot as plt
 import os, os.path
 import sys
-
-# Notes: 'face.xml' is 'opencv/data/haarcascades/haarcascade_frontalface_default.xml'
-
-frontFaceCascade = cv2.CascadeClassifier('frontFace_default.xml')
-profileFaceCascade = cv2.CascadeClassifier('profileFace.xml')
-
-# Notes : 'JoYuris' element is file directory
-# raw image file is saved 'raw' dir and cropped file is saved 'cropped' dir
-# ex) raw image : './Yuri/raw/1.jpg', cropped image : './Yuri/cropped/1.jpg' 
-
-JoYuris =['Yuri', 'Chaewon', 'Yaena']
+from PIL import Image
+from PIL import GifImagePlugin
+import face_recognition as FR
+import magic
+import shutil
+JoYuris =['Yuri', 'Chaewon', 'Yena']
 for member in JoYuris:
-    path = './rawImage/' + member + '/'
+    newImageName = ''
+    path = '../Dataset/Raw/' + member + '/'
+    savePath = '../Dataset/Train/' + member + '/'
+    failPath = '../Dataset/Fail/' + member + '/'
     imageList = os.listdir(path)
-    print(format(imageList))
     cnt = 0
     for imageName in imageList:
         print("path+imageName : ",path+imageName)
-        if imageName.strip().split('.')[-1] != 'jpg':
+        if os.path.isfile(savePath+imageName) == True:
             continue
-        image = cv2.imread(path+imageName)
-        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        frontFaces = frontFaceCascade.detectMultiScale(gray, 1.3, 5)
-        profileFaces = profileFaceCascade.detectMultiScale(gray, 1.3, 5)
-        if len(frontFaces) > 0:
-            faces = frontFaces
-        elif len(profileFaces) > 0:
-            faces = profileFaces
+        extention = magic.from_file(path+imageName).split()[0].upper()
+        if extention == 'GIF':
+            imageObject = Image.open(path+imageName)
+            imageObject.seek(0) 
+            imageObject = imageObject.convert('RGB')
+            image = np.array(imageObject)
+        elif extention != 'JPEG' and extention != 'PNG':
+            continue
         else:
+            image = FR.load_image_file(path+imageName)
+        faces = FR.face_locations(image, number_of_times_to_upsample=0, model="hog")
+        if len(faces) != 1 :
+            shutil.move(path+imageName, failPath+imageName)
             continue
-            
-        x, y, w, h = faces[0]
-        print(x, y, w, h)
-        
-        cnt = cnt + 1
-        tcnt = 0
-        crop  = image[y:y+h, x:x+w]
-        resized= cv2.resize(crop, (128, 128),interpolation = cv2.INTER_CUBIC)
-        #plt.imshow(cv2.cvtColor(resized, cv2.COLOR_BGR2RGB))
-        #plt.show()
-        cv2.imwrite('./trainImage/' +member + '/' +str(cnt)+'-'+str(tcnt)+'.jpg', resized)
-        
-
+        for T, R, B, L in faces:
+            cnt = cnt + 1
+            tcnt = 0
+            crop  = image[T:B, L:R]
+            resized = Image.fromarray(crop).resize((256,256), Image.BICUBIC)
+            print(type(resized))
+            newImageName = savePath + imageName.split('.')[0] + '.jpg'
+            print(newImageName)
+            resized.save(newImageName)
